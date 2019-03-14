6d0f45c3f26560231ecee3b9694f80eee1e4ef05
Sami <sami@seluxit.com>
2019-03-14 14:05:37 +0100

    added tracer to wappsto class

--- wappsto/index.js
+++ wappsto/index.js
@@ -4,6 +4,7 @@ const querystring = require('querystring');
 const Models = require('../models');
 const Stream = require('../stream');
 const Util = require('../util');
+const Tracer = require('../tracer');
 
 const Request = require('./request');
 
@@ -23,6 +24,7 @@ class Wappsto {
     }
     this[_wappstoModels] = new Models(this[_requestInstance]);
     this[_Stream] = Stream;
+    this.Tracer = Tracer;
   }
 
   get util() {
